6cfd295c96e2c5b502d30b458de632788f884353
Eileen McFarland <eileenm@gmail.com>
2016-03-14 13:26:39 -0500

    begin creating Member class with initialize method

---
+++ Member.rb
@@ -0,0 +1,14 @@
+class Member
+  attr_reader :member_accounts
+
+  def initialize(first_name, last_name)
+    @first_name = first_name
+    @last_name = last_name
+    @member_accounts = []
+  end
+
+  def open_new_account
+    # need to fill in method
+  end
+
+end
