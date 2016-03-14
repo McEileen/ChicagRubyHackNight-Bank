e9de08106502243766c72cb639b9387b2c6bfa20
Eileen McFarland <eileenm@gmail.com>
2016-03-14 13:23:49 -0500

    pseudocode and begin creating bank class

---
+++ Bank.rb
@@ -0,0 +1,33 @@
+# PSEUDOCODE
+# Create a bank class
+# 	holds list of members
+#	methods to add member, display members, search for member
+#	method to calculate total money for all accounts together
+# Create an account class
+#	has default value of zero
+#	method to add/withdraw funds
+#	above method will create a new instance of the transaction class
+#	has a transaction history
+# Create a member class
+#	has a first and last name
+#	method to open account
+# Create a transaction class
+#	date and time variable (datetime stamp)
+#	amount of the transaction
+#	has an id
+
+require_relative 'member'
+class Bank
+  attr_reader :list_of_members
+
+  def initialize
+    @list_of_members = []
+  end
+
+  def add_member(first_name, last_name)
+    @list_of_members << Member.new(first_name, last_name)
+  end
+
+
+
+end
