75c7a8c2f0597d8feedda2d5023fe611a532f6fb
Eileen McFarland <eileenm@gmail.com>
2016-03-14 13:27:01 -0500

    write first tests for Bank class

---
+++ spec/bank_spec.rb
@@ -0,0 +1,25 @@
+require_relative '../Bank'
+
+describe Bank do
+  let(:new_bank) { Bank.new }
+
+  describe "#list_of_members" do
+    it 'should be readable' do
+      expect(new_bank.list_of_members.length).to eq(0)
+    end
+    it 'should be equal to zero when a new bank is created' do
+      expect(new_bank.list_of_members.length).to eq(0)
+    end
+
+  end
+
+  describe '#add_member' do
+    it 'should make one more be a customer of the bank' do
+      new_bank.add_member("Bob", "Smith")
+      list_of_members = new_bank.list_of_members
+      expect(list_of_members.length).to eq(1)
+    end
+  end
+
+
+end
