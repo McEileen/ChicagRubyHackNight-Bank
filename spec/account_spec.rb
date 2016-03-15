113fa194800db77d429c46dc697e758ef5a97206
Eileen McFarland <eileenm@gmail.com>
2016-03-15 16:57:03 -0500

    test for account initial value of zero

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,6 +1,16 @@
 require_relative '../Account'
 
 describe Account do
-  let(:new_account) {Account.new}
+
+  describe 'it has initial variables' do
+    empty_account = Account.new
+    it 'has an initial numeric value' do
+      expect(empty_account.value).to be_a(Fixnum)
+    end
+
+    it 'has an initial value of 0' do
+      expect(empty_account.value).to eq(0)
+    end
+  end
 
 end
